17890133c0aff73d8e06d304b645f3e96970607a
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-25 00:59:07 +0900

    create fetchAsyncGetSegments

--- src/features/vehicleSlice.ts
+++ src/features/vehicleSlice.ts
@@ -1,4 +1,17 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
 import axios from 'axios'
+import { RootState } from '../app/store'
 
 const apiUrl = 'http://localhost:8000/'
+
+export const fetchAsyncGetSegments = createAsyncThunk(
+  'segment/get',
+  async () => {
+    const res = await axios.get(`${apiUrl}api/segments/`, {
+      headers: {
+        Authorization: `token ${localStorage.token}`,
+      },
+    })
+    return res.data
+  }
+)
