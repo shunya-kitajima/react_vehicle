13884d3023915b2375456ffe84812a8a400a7c43
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-25 00:53:13 +0900

    modify

--- src/features/authSlice.ts
+++ src/features/authSlice.ts
@@ -1,6 +1,6 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
 import axios from 'axios'
-import { RootState, AppThunk } from '../app/store'
+import { RootState } from '../app/store'
 
 const apiUrl = 'http://localhost:8000/'
 
