b0f691322146e0959d0e6608a4a63803e4300890
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-25 00:03:00 +0900

    import modules and rap by browserrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import styles from './App.module.css'
+import Auth from './components/Auth'
+import MainPage from './components/MainPage'
 
 const App: React.FC = () => {
-  return <div className="App"></div>
+  return (
+    <BrowserRouter>
+      <div className={styles.app__root}></div>
+    </BrowserRouter>
+  )
 }
 
 export default App
