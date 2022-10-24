b8f2435b89e56475847270c39eee15881fdefed8
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-25 00:22:46 +0900

    modify

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,9 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit'
-import counterReducer from '../features/counterSlice'
+import authReducer from '../features/authSlice'
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    auth: authReducer,
   },
 })
 
