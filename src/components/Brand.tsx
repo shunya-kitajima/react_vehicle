96e20a4efb2262699b3348879e06254f69e39154
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-25 21:05:03 +0900

    createUpdateBrand

--- src/components/Brand.tsx
+++ src/components/Brand.tsx
@@ -9,6 +9,33 @@ import {
 } from '../features/vehicleSlice'
 
 const Brand: React.FC = () => {
+  const dispatch = useAppDispatch()
+  const brands = useAppSelector(selectBrands)
+  const editedBrand = useAppSelector(selectEditedBrand)
+  const [successMsg, setSuccessMsg] = useState('')
+  const {
+    fetchAsyncGetBrands,
+    fetchAsyncCreateBrand,
+    fetchAsyncUpdateBrand,
+    fetchAsyncDeleteBrand,
+  } = fetchBrand()
+
+  const createUpdateBrand = async () => {
+    if (editedBrand.id === 0) {
+      const result = await dispatch(
+        fetchAsyncCreateBrand({ brand_name: editedBrand.brand_name })
+      )
+      dispatch(editBrand({ id: 0, brand_name: '' }))
+      if (fetchAsyncCreateBrand.fulfilled.match(result))
+        setSuccessMsg('Created brand!')
+    } else {
+      const result = await dispatch(fetchAsyncUpdateBrand(editedBrand))
+      dispatch(editBrand({ id: 0, brand_name: '' }))
+      if (fetchAsyncUpdateBrand.fulfilled.match(result))
+        setSuccessMsg('Updated brand!')
+    }
+  }
+
   return <div>Brand</div>
 }
 
