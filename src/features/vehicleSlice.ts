4bfa568c0cc75f2a51e172dffefc7676b77dc4b0
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-25 10:15:10 +0900

    create segment function

--- src/features/vehicleSlice.ts
+++ src/features/vehicleSlice.ts
@@ -19,7 +19,7 @@ export const fetchAsyncGetSegments = createAsyncThunk(
 
 export const fetchAsyncCreateSegment = createAsyncThunk(
   'segment/post',
-  async (segment) => {
+  async (segment: SegmentType) => {
     const res = await axios.post(`${apiUrl}api/segments/`, segment, {
       headers: {
         'Content-Type': 'application/json',
@@ -32,7 +32,39 @@ export const fetchAsyncCreateSegment = createAsyncThunk(
 
 export const fetchAsyncUpdateSegment = createAsyncThunk(
   'segment/put',
-  async (segment) => {
-    const res = await axios.put(`${apiUrl}api/segments/${segment.id}/`, segment)
+  async (segment: SegmentType) => {
+    const res = await axios.put(
+      `${apiUrl}api/segments/${segment.id}/`,
+      segment,
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `token ${localStorage.token}`,
+        },
+      }
+    )
+    return res.data
   }
 )
+
+export const fetchAsyncDeleteSegment = createAsyncThunk(
+  'segment/delete',
+  async (id: string) => {
+    const res = await axios.delete(`${apiUrl}api/segments/${id}/`, {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `token ${localStorage.token}`,
+      },
+    })
+    return res.data
+  }
+)
+
+export const fetchAsyncGetBrand = createAsyncThunk('brand/get', async () => {
+  const res = await axios.get(`${apiUrl}api/brands/`, {
+    headers: {
+      Authorization: `token ${localStorage.token}`,
+    },
+  })
+  return res.data
+})
