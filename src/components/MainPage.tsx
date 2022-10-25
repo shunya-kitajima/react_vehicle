9d770b0450f3d0aea990e3219fde2d8a3fa47c25
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-25 15:48:10 +0900

    create logout button

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const MainPage: React.FC = () => {
-  return <div>Welcome to MainPage</div>
+  const navigate = useNavigate()
+
+  const logout = () => {
+    localStorage.removeItem('token')
+    navigate('/')
+  }
+
+  return (
+    <div>
+      <button data-testid="btn-logout" onClick={logout}>
+        Logout
+      </button>
+    </div>
+  )
 }
 
 export default MainPage
