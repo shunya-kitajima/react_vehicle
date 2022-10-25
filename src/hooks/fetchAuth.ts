3c60aa36485a0b13151d98a7c309312cc6468cdf
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-25 11:06:05 +0900

    create fetch functions

---
+++ src/hooks/fetchAuth.ts
@@ -0,0 +1,35 @@
+import { createAsyncThunk } from '@reduxjs/toolkit'
+import axios from 'axios'
+
+const apiUrl = 'http://localhost:8000/'
+
+export const fetchAuth = () => {
+  const fetchAsyncLogin = createAsyncThunk('login/post', async (auth) => {
+    const res = await axios.post(`${apiUrl}api/auth/`, auth, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    return res.data
+  })
+
+  const fetchAsyncRegister = createAsyncThunk('register/post', async (auth) => {
+    const res = await axios.post(`${apiUrl}api/create/`, auth, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    return res.data
+  })
+
+  const fetchAsyncGetProfile = createAsyncThunk('profile/get', async () => {
+    const res = await axios.get(`${apiUrl}api/profile/`, {
+      headers: {
+        Authorization: `token ${localStorage.token}`,
+      },
+    })
+    return res.data
+  })
+
+  return { fetchAsyncLogin, fetchAsyncRegister, fetchAsyncGetProfile }
+}
