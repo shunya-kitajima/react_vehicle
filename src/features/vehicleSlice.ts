f75de6bbb64d93f950f8f2813f8dd79bfba9b7b1
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-25 11:23:03 +0900

    create reducers

--- src/features/vehicleSlice.ts
+++ src/features/vehicleSlice.ts
@@ -1,164 +1,76 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
-import axios from 'axios'
+import { createSlice } from '@reduxjs/toolkit'
 import { RootState } from '../app/store'
-import { SegmentType, BrandType, VehicleType } from '../../types/types'
+import {
+  SegmentType,
+  BrandType,
+  VehicleType,
+  EditedVehicleType,
+} from '../../types/types'
 
-const apiUrl = 'http://localhost:8000/'
+export interface VehicleState {
+  segments: SegmentType[]
+  brands: BrandType[]
+  vehicles: VehicleType[]
+  editedSegment: SegmentType
+  editedBrand: BrandType
+  editedVehicle: EditedVehicleType
+}
 
-export const fetchAsyncGetSegments = createAsyncThunk(
-  'segment/get',
-  async () => {
-    const res = await axios.get(`${apiUrl}api/segments/`, {
-      headers: {
-        Authorization: `token ${localStorage.token}`,
-      },
-    })
-    return res.data
-  }
-)
-
-export const fetchAsyncCreateSegment = createAsyncThunk(
-  'segment/post',
-  async (segment: Omit<SegmentType, 'id'>) => {
-    const res = await axios.post(`${apiUrl}api/segments/`, segment, {
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `token ${localStorage.token}`,
-      },
-    })
-    return res.data
-  }
-)
-
-export const fetchAsyncUpdateSegment = createAsyncThunk(
-  'segment/put',
-  async (segment: SegmentType) => {
-    const res = await axios.put(
-      `${apiUrl}api/segments/${segment.id}/`,
-      segment,
-      {
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `token ${localStorage.token}`,
-        },
-      }
-    )
-    return res.data
-  }
-)
-
-export const fetchAsyncDeleteSegment = createAsyncThunk(
-  'segment/delete',
-  async (id: string) => {
-    const res = await axios.delete(`${apiUrl}api/segments/${id}/`, {
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `token ${localStorage.token}`,
-      },
-    })
-    return res.data
-  }
-)
-
-export const fetchAsyncGetBrands = createAsyncThunk('brand/get', async () => {
-  const res = await axios.get(`${apiUrl}api/brands/`, {
-    headers: {
-      Authorization: `token ${localStorage.token}`,
+const initialState: VehicleState = {
+  segments: [
+    {
+      id: 0,
+      segment_name: '',
     },
-  })
-  return res.data
+  ],
+  brands: [
+    {
+      id: 0,
+      brand_name: '',
+    },
+  ],
+  vehicles: [
+    {
+      id: 0,
+      vehicle_name: '',
+      release_year: 2020,
+      price: 0.0,
+      segment: 0,
+      brand: 0,
+      segment_name: '',
+      brand_name: '',
+    },
+  ],
+  editedSegment: {
+    id: 0,
+    segment_name: '',
+  },
+  editedBrand: {
+    id: 0,
+    brand_name: '',
+  },
+  editedVehicle: {
+    id: 0,
+    vehicle_name: '',
+    release_year: 2020,
+    price: 0.0,
+    segment: 0,
+    brand: 0,
+  },
+}
+
+export const vehicleSlice = createSlice({
+  name: 'vehicle',
+  initialState,
+  reducers: {
+    editSegment: (state, action) => {
+      state.editedSegment = action.payload
+    },
+    editBrand: (state, action) => {
+      state.editedBrand = action.payload
+    },
+    editVehicle: (state, action) => {
+      state.editedVehicle = action.payload
+    },
+  },
 })
-
-export const fetchAsyncCreateBrand = createAsyncThunk(
-  'brand/post',
-  async (brand: Omit<BrandType, 'id'>) => {
-    const res = await axios.post(`${apiUrl}api/brands/`, brand, {
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `token ${localStorage.token}`,
-      },
-    })
-    return res.data
-  }
-)
-
-export const fetchAsyncUpdateBrand = createAsyncThunk(
-  'brand/put',
-  async (brand: BrandType) => {
-    const res = await axios.put(`${apiUrl}api/brands/${brand.id}/`, brand, {
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `token ${localStorage.token}`,
-      },
-    })
-    return res.data
-  }
-)
-
-export const fetchAsyncDeleteBrand = createAsyncThunk(
-  'brand/delete',
-  async (id: string) => {
-    const res = await axios.delete(`${apiUrl}api/brands/${id}/`, {
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `token ${localStorage.token}`,
-      },
-    })
-    return res.data
-  }
-)
-
-export const fetchAsyncGetVehicles = createAsyncThunk(
-  'vehicle/get',
-  async () => {
-    const res = await axios.get(`${apiUrl}api/vehicles/`, {
-      headers: {
-        Authorization: `token ${localStorage.token}`,
-      },
-    })
-    return res.data
-  }
-)
-
-export const fetchAsyncCreateVehicle = createAsyncThunk(
-  'vehicle/post',
-  async (vehicle: Omit<VehicleType, 'id'>) => {
-    const res = await axios.post(`${apiUrl}api/vehicles/`, vehicle, {
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `token ${localStorage.token}`,
-      },
-    })
-    return res.data
-  }
-)
-
-export const fetchAsyncUpdateVehicle = createAsyncThunk(
-  'vehicle/put',
-  async (vehicle: VehicleType) => {
-    const res = await axios.put(
-      `${apiUrl}api/vehicles/${vehicle.id}/`,
-      vehicle,
-      {
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `token ${localStorage.token}`,
-        },
-      }
-    )
-    return res.data
-  }
-)
-
-export const fetchAsyncDeleteVehicle = createAsyncThunk(
-  'vehicle/delete',
-  async (id: string) => {
-    const res = await axios.delete(`${apiUrl}api/vehicles/${id}/`, {
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `token ${localStorage.token}`,
-      },
-    })
-    return res.data
-  }
-)
