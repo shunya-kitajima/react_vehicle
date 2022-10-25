a4ab4afa6940c165e984fdda50a5980d4df521b4
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-25 21:46:12 +0900

    import modules

--- src/components/Vehicle.tsx
+++ src/components/Vehicle.tsx
@@ -1,4 +1,14 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useAppDispatch, useAppSelector } from '../app/hooks'
+import styles from './Vehicle.module.css'
+import { fetchVehicle } from '../hooks/fetchVehicle'
+import {
+  editVehicle,
+  selectVehicles,
+  selectEditedVehicle,
+  selectSegments,
+  selectBrands,
+} from '../features/vehicleSlice'
 
 const Vehicle: React.FC = () => {
   return <div>Vehicle</div>
