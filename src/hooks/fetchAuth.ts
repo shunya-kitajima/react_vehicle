30150487f57089166a16844eb0b45c8658feeff8
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-25 15:16:25 +0900

    add type for paramater

--- src/hooks/fetchAuth.ts
+++ src/hooks/fetchAuth.ts
@@ -1,26 +1,33 @@
 import { createAsyncThunk } from '@reduxjs/toolkit'
 import axios from 'axios'
+import { AuthType } from '../../types/types'
 
 const apiUrl = 'http://localhost:8000/'
 
 export const fetchAuth = () => {
-  const fetchAsyncLogin = createAsyncThunk('login/post', async (auth) => {
-    const res = await axios.post(`${apiUrl}api/auth/`, auth, {
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
-    return res.data
-  })
+  const fetchAsyncLogin = createAsyncThunk(
+    'login/post',
+    async (auth: AuthType) => {
+      const res = await axios.post(`${apiUrl}api/auth/`, auth, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+      return res.data
+    }
+  )
 
-  const fetchAsyncRegister = createAsyncThunk('register/post', async (auth) => {
-    const res = await axios.post(`${apiUrl}api/create/`, auth, {
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
-    return res.data
-  })
+  const fetchAsyncRegister = createAsyncThunk(
+    'register/post',
+    async (auth: AuthType) => {
+      const res = await axios.post(`${apiUrl}api/create/`, auth, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+      return res.data
+    }
+  )
 
   const fetchAsyncGetProfile = createAsyncThunk('profile/get', async () => {
     const res = await axios.get(`${apiUrl}api/profile/`, {
