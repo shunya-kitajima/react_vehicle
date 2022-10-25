b8d96460d3ff0dc40e4f090db5dffcae761fea6b
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-25 11:07:50 +0900

    modify

--- src/features/authSlice.ts
+++ src/features/authSlice.ts
@@ -1,8 +1,8 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
-import axios from 'axios'
+import { createSlice } from '@reduxjs/toolkit'
 import { RootState } from '../app/store'
+import { fetchAuth } from '../hooks/fetchAuth'
 
-const apiUrl = 'http://localhost:8000/'
+const { fetchAsyncLogin, fetchAsyncGetProfile } = fetchAuth()
 
 export interface AuthState {
   profile: {
@@ -17,39 +17,6 @@ const initialState: AuthState = {
     username: '',
   },
 }
-
-export const fetchAsyncLogin = createAsyncThunk('login/post', async (auth) => {
-  const res = await axios.post(`${apiUrl}api/auth/`, auth, {
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  })
-  return res.data
-})
-
-export const fetchAsyncRegister = createAsyncThunk(
-  'register/post',
-  async (auth) => {
-    const res = await axios.post(`${apiUrl}api/create/`, auth, {
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
-    return res.data
-  }
-)
-
-export const fetchAsyncGetProfile = createAsyncThunk(
-  'profile/get',
-  async () => {
-    const res = await axios.get(`${apiUrl}api/profile/`, {
-      headers: {
-        Authorization: `token ${localStorage.token}`,
-      },
-    })
-    return res.data
-  }
-)
 
 export const authSlice = createSlice({
   name: 'auth',
