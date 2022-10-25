f5699cafcd3965a968cf04f009facfc5643171eb
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-25 16:04:15 +0900

    import modules

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,5 +1,13 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useAppDispatch, useAppSelector } from '../app/hooks'
+import { Grid } from '@material-ui/core'
 import { useNavigate } from 'react-router-dom'
+import styles from './MainPage.module.css'
+import { fetchAuth } from '../hooks/fetchAuth'
+import { selectProfile } from '../features/authSlice'
+import Segment from './Segment'
+import Brand from './Brand'
+import Vehicle from './Vehicle'
 
 const MainPage: React.FC = () => {
   const navigate = useNavigate()
