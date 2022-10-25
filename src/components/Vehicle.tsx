b1da0dc78cf740a92555c0ca44e18555277267c9
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-25 21:59:00 +0900

    create options and set useEffect

--- src/components/Vehicle.tsx
+++ src/components/Vehicle.tsx
@@ -24,6 +24,27 @@ const Vehicle: React.FC = () => {
     fetchAsyncDeleteVehicle,
   } = fetchVehicle()
 
+  const segmentOptions = segments?.map((segment) => (
+    <option key={segment.id} value={segment.id}>
+      {segment.segment_name}
+    </option>
+  ))
+  const brandOptions = brands?.map((brand) => (
+    <option key={brand.id} value={brand.id}>
+      {brand.brand_name}
+    </option>
+  ))
+
+  useEffect(() => {
+    const fetchBootLoader = async () => {
+      const result = await dispatch(fetchAsyncGetVehicles())
+      if (fetchAsyncGetVehicles.rejected.match(result))
+        setSuccessMsg('Get error!')
+    }
+    fetchBootLoader()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
   return <div>Vehicle</div>
 }
 
