37d735227416279f506b3ab6dc42703efc405ab4
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-25 21:32:05 +0900

    create state, useEffect, functions, display component

--- src/components/Brand.tsx
+++ src/components/Brand.tsx
@@ -36,7 +36,68 @@ const Brand: React.FC = () => {
     }
   }
 
-  return <div>Brand</div>
+  useEffect(() => {
+    const fetchBootLoader = async () => {
+      const result = await dispatch(fetchAsyncGetBrands())
+      if (fetchAsyncGetBrands.rejected.match(result))
+        setSuccessMsg('Get error!')
+    }
+    fetchBootLoader()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  return (
+    <>
+      <h3 data-testid="h3-brand">Brand</h3>
+      <span className={styles.brand__status}>{successMsg}</span>
+      <div>
+        <input
+          type="text"
+          placeholder="new brand name"
+          value={editedBrand.brand_name}
+          onChange={(e) =>
+            dispatch(editBrand({ ...editedBrand, brand_name: e.target.value }))
+          }
+        />
+        <button
+          data-testid="btn-post"
+          disabled={!editedBrand.brand_name}
+          onClick={createUpdateBrand}
+        >
+          {editedBrand.id === 0 ? 'Create' : 'Update'}
+        </button>
+        <ul>
+          {brands.map((brand) => (
+            <li className={styles.brand__item} key={brand.id}>
+              <span data-testid={`list-${brand.id}`}>{brand.brand_name}</span>
+              <div>
+                <button
+                  data-testid={`delete-brand-${brand.id}`}
+                  onClick={async () => {
+                    const result = await dispatch(
+                      fetchAsyncDeleteBrand(editedBrand.id)
+                    )
+                    if (fetchAsyncDeleteBrand.fulfilled.match(result))
+                      setSuccessMsg('Delete brand!')
+                  }}
+                >
+                  delete
+                </button>
+                <button
+                  data-testid={`edit-brand-${brand.id}`}
+                  onClick={() => {
+                    dispatch(editBrand(brand))
+                  }}
+                >
+                  edit
+                </button>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  )
 }
 
 export default Brand
