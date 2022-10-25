60ef72b5ea085ebf8b2fb2c483fec2beb404908f
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-25 20:58:17 +0900

    import modules

--- src/components/Brand.tsx
+++ src/components/Brand.tsx
@@ -1,4 +1,12 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useAppDispatch, useAppSelector } from '../app/hooks'
+import styles from './Brand.module.css'
+import { fetchBrand } from '../hooks/fetchBrand'
+import {
+  editBrand,
+  selectBrands,
+  selectEditedBrand,
+} from '../features/vehicleSlice'
 
 const Brand: React.FC = () => {
   return <div>Brand</div>
