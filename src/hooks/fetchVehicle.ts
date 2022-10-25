324f1ffa00e07c84468c6def5e2637990ae882e4
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-25 11:00:33 +0900

    create functions

---
+++ src/hooks/fetchVehicle.ts
@@ -0,0 +1,66 @@
+import { createAsyncThunk } from '@reduxjs/toolkit'
+import axios from 'axios'
+import { VehicleType } from '../../types/types'
+
+const apiUrl = 'http://localhost:8000/'
+
+export const fetchVehicle = () => {
+  const fetchAsyncGetVehicles = createAsyncThunk('vehicle/get', async () => {
+    const res = await axios.get(`${apiUrl}api/vehicles/`, {
+      headers: {
+        Authorization: `token ${localStorage.token}`,
+      },
+    })
+    return res.data
+  })
+
+  const fetchAsyncCreateVehicle = createAsyncThunk(
+    'vehicle/post',
+    async (vehicle: Omit<VehicleType, 'id'>) => {
+      const res = await axios.post(`${apiUrl}api/vehicles/`, vehicle, {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `token ${localStorage.token}`,
+        },
+      })
+      return res.data
+    }
+  )
+
+  const fetchAsyncUpdateVehicle = createAsyncThunk(
+    'vehicle/put',
+    async (vehicle: VehicleType) => {
+      const res = await axios.put(
+        `${apiUrl}api/vehicles/${vehicle.id}/`,
+        vehicle,
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `token ${localStorage.token}`,
+          },
+        }
+      )
+      return res.data
+    }
+  )
+
+  const fetchAsyncDeleteVehicle = createAsyncThunk(
+    'vehicle/delete',
+    async (id: string) => {
+      const res = await axios.delete(`${apiUrl}api/vehicles/${id}/`, {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `token ${localStorage.token}`,
+        },
+      })
+      return res.data
+    }
+  )
+
+  return {
+    fetchAsyncGetVehicles,
+    fetchAsyncCreateVehicle,
+    fetchAsyncUpdateVehicle,
+    fetchAsyncDeleteVehicle,
+  }
+}
