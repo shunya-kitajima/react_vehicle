799edda7358ea731f280e424895c7047642cb98b
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-26 16:57:52 +0900

    create mock functin and handlers, server

--- src/tests/Auth.test.tsx
+++ src/tests/Auth.test.tsx
@@ -12,3 +12,30 @@ import { setupServer } from 'msw/node'
 import { fetchAuth } from '../hooks/fetchAuth'
 import authReducer from '../features/authSlice'
 import Auth from '../components/Auth'
+
+const mockNavigate = jest.fn()
+
+jest.mock('react-router-dom', () => ({
+  useNavigate: () => mockNavigate,
+}))
+
+const handlers = [
+  rest.post('http://localhost:8000/api/auth/', (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json({ token: 'abc123' }))
+  }),
+  rest.post('http://localhost:8000/api/create/', (req, res, ctx) => {
+    return res(ctx.status(200))
+  }),
+]
+
+const server = setupServer(...handlers)
+beforeAll(() => {
+  server.listen()
+})
+afterEach(() => {
+  server.resetHandlers()
+  cleanup()
+})
+afterAll(() => {
+  server.close()
+})
