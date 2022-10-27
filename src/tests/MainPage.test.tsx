7bdd98b7a3c09cd53387c7a93490ef6e80041f67
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-27 11:00:53 +0900

    add tests

--- src/tests/MainPage.test.tsx
+++ src/tests/MainPage.test.tsx
@@ -11,7 +11,6 @@ import { setupServer } from 'msw/node'
 import authReducer from '../features/authSlice'
 import vehicleReducer from '../features/vehicleSlice'
 import MainPage from '../components/MainPage'
-import Auth from '../components/Auth'
 
 const mockNavigate = jest.fn()
 
@@ -20,7 +19,7 @@ jest.mock('react-router-dom', () => ({
 }))
 
 const handlers = [
-  rest.post('http://localhost:8000/api/profile/', (req, res, ctx) => {
+  rest.get('http://localhost:8000/api/profile/', (req, res, ctx) => {
     return res(ctx.status(200), ctx.json({ id: 1, username: 'test user' }))
   }),
   rest.get('http://localhost:8000/api/segments/', (req, res, ctx) => {
@@ -44,4 +43,44 @@ afterEach(() => {
 })
 afterAll(() => {
   server.close()
+})
+
+describe('MainPage Component Test Cases', () => {
+  let store: Store<any, AnyAction>
+  beforeEach(() => {
+    store = configureStore({
+      reducer: {
+        auth: authReducer,
+        vehicle: vehicleReducer,
+      },
+    })
+  })
+  it('1: Should render all the elements correctly', async () => {
+    render(
+      <Provider store={store}>
+        <MainPage />
+      </Provider>
+    )
+    expect(screen.getByTestId('span-title')).toBeTruthy()
+    expect(screen.getByTestId('btn-logout')).toBeTruthy()
+  })
+  it('2: Should route to Auth page when logout button pressed', async () => {
+    render(
+      <Provider store={store}>
+        <MainPage />
+      </Provider>
+    )
+    await userEvent.click(screen.getByTestId('btn-logout'))
+    expect(mockNavigate).toBeCalledWith('/')
+    expect(mockNavigate).toHaveBeenCalledTimes(1)
+  })
+  it('3: Should render logged in user name', async () => {
+    render(
+      <Provider store={store}>
+        <MainPage />
+      </Provider>
+    )
+    expect(screen.queryByText('test user')).toBeNull()
+    expect(await screen.findByText('test user')).toBeInTheDocument()
+  })
 })
