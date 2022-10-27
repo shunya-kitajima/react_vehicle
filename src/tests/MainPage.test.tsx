dfdd918bacec9e03d50d151e3c41fadc81460257
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-27 10:24:17 +0900

    import modules

---
+++ src/tests/MainPage.test.tsx
@@ -0,0 +1,12 @@
+/**
+ * @jest-environment jsdom
+ */
+import '@testing-library/jest-dom/extend-expect'
+import { Provider } from 'react-redux'
+import { AnyAction, configureStore, Store } from '@reduxjs/toolkit'
+import { render, screen, cleanup } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { rest } from 'msw'
+import { setupServer } from 'msw/node'
+import MainPage from '../components/MainPage'
+import Auth from '../components/Auth'
