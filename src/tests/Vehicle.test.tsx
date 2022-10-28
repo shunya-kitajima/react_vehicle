8ae653c9210f5558af0500961590548a8d7aa451
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-28 17:41:30 +0900

    set handlers and describe

---
+++ src/tests/Vehicle.test.tsx
@@ -0,0 +1,139 @@
+/**
+ * @jest-environment jsdom
+ */
+import '@testing-library/jest-dom/extend-expect'
+import { Provider } from 'react-redux'
+import { AnyAction, configureStore, Store } from '@reduxjs/toolkit'
+import { render, screen, cleanup } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { rest } from 'msw'
+import { setupServer } from 'msw/node'
+import vehicleReducer from '../features/vehicleSlice'
+import Segment from '../components/Segment'
+import Brand from '../components/Brand'
+import Vehicle from '../components/Vehicle'
+
+const handlers = [
+  rest.get('http://localhost:8000/api/segments/', (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json([
+        { id: 1, segment_name: 'K-CAR' },
+        { id: 2, segment_name: 'EV' },
+      ])
+    )
+  }),
+  rest.get('http://localhost:8000/api/brands/', (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json([
+        { id: 1, brand_name: 'Toyota' },
+        { id: 2, brand_name: 'Tesla' },
+      ])
+    )
+  }),
+  rest.delete('http://localhost:8000/api/segments/1/', (req, res, ctx) => {
+    return res(ctx.status(200))
+  }),
+  rest.delete('http://localhost:8000/api/segments/2/', (req, res, ctx) => {
+    return res(ctx.status(200))
+  }),
+  rest.delete('http://localhost:8000/api/brands/1/', (req, res, ctx) => {
+    return res(ctx.status(200))
+  }),
+  rest.delete('http://localhost:8000/api/brands/2/', (req, res, ctx) => {
+    return res(ctx.status(200))
+  }),
+  rest.get('http://localhost:8000/api/vehicles/', (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json([
+        {
+          id: 1,
+          vehicle_name: 'SQ7',
+          release_year: 2019,
+          price: 300.12,
+          segment: 1,
+          brand: 1,
+          segment_name: 'K-CAR',
+          brand_name: 'Toyota',
+        },
+        {
+          id: 2,
+          vehicle_name: 'MODEL S',
+          release_year: 2020,
+          price: 400.12,
+          segment: 2,
+          brand: 2,
+          segment_name: 'EV',
+          brand_name: 'Tesla',
+        },
+      ])
+    )
+  }),
+  rest.post('http://localhost:8000/api/vehicles/', (req, res, ctx) => {
+    return res(
+      ctx.status(201),
+      ctx.json({
+        id: 3,
+        vehicle_name: 'MODEL X',
+        release_year: 2019,
+        price: 350.12,
+        segment: 2,
+        brand: 2,
+        segment_name: 'EV',
+        brand_name: 'Tesla',
+      })
+    )
+  }),
+  rest.put('http://localhost:8000/api/vehicles/1/', (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json({
+        id: 1,
+        vehicle_name: 'new SQ7',
+        release_year: 2010,
+        price: 350.12,
+        segment: 2,
+        brand: 2,
+        segment_name: 'EV',
+        brand_name: 'Tesla',
+      })
+    )
+  }),
+  rest.put('http://localhost:8000/api/vehicles/2/', (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json({
+        id: 2,
+        vehicle_name: 'new MODEL S',
+        release_year: 2018,
+        price: 450.12,
+        segment: 1,
+        brand: 1,
+        segment_name: 'K-CAR',
+        brand_name: 'Toyota',
+      })
+    )
+  }),
+  rest.delete('http://localhost:8000/api/vehicles/1/', (req, res, ctx) => {
+    return res(ctx.status(200))
+  }),
+  rest.delete('http://localhost:8000/api/vehicles/2/', (req, res, ctx) => {
+    return res(ctx.status(200))
+  }),
+]
+
+const server = setupServer(...handlers)
+beforeAll(() => {
+  server.listen()
+})
+afterEach(() => {
+  server.resetHandlers()
+  cleanup()
+})
+afterAll(() => {
+  server.close()
+})
+
+describe('Vehicle Component Test Cases', () => {})
